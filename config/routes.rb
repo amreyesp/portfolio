d6beaebe4dfcfec062f9d2986a088716d4288156
amreyesp <amreyesp@unal.edu.co>
2019-07-05 11:48:32 -0500

    implement sdir menu and responsive design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#welcome', as: :welcome
+  root 'pages#home', as: :home
   get '/portfolio' => 'pages#portfolio', as: :portfolio
 
 end
